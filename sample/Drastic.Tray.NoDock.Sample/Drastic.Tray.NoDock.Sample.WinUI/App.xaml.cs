86b42b12778faef7be467a92561372f4ec37af51
Timothy Miller <innerlogic4321@gmail.com>
2022-12-27 21:40:58 +0900

    Include WinUI No Taskbar Sample

---
+++ sample/Drastic.Tray.NoDock.Sample/Drastic.Tray.NoDock.Sample.WinUI/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Drastic.TrayWindow;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Drastic.Tray.NoDock.Sample.WinUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private TrayIcon icon;
+        private WinUITrayWindow window;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+
+            var trayImage = new TrayImage(System.Drawing.Image.FromStream(GetResourceFileContent("TrayIcon.ico")!));
+            var menuItems = new List<TrayMenuItem>
+            {
+                new TrayMenuItem("Quit!", trayImage, async () => { 
+                    Application.Current.Exit(); 
+                }),
+                new TrayMenuItem("Hello!", trayImage, async () => { }),
+                new TrayMenuItem("From!", trayImage, async () => { }),
+                new TrayMenuItem("Windows!", trayImage, async () => { }),
+            };
+
+            this.icon = new TrayIcon("Tray Icon", trayImage, menuItems);
+            this.window = new MainWindow(icon, new TrayWindowOptions());
+            this.icon.LeftClicked += (object? sender, TrayClickedEventArgs e) =>
+            {
+                this.window.ToggleVisibility();
+            };
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+        }
+
+        private Window? m_window;
+
+        /// <summary>
+        /// Get Resource File Content via FileName.
+        /// </summary>
+        /// <param name="fileName">Filename.</param>
+        /// <returns>Stream.</returns>
+        public static Stream? GetResourceFileContent(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Drastic.Tray.NoDock.Sample.WinUI." + fileName;
+            if (assembly is null)
+            {
+                return null;
+            }
+
+            return assembly.GetManifestResourceStream(resourceName);
+        }
+    }
+}
--- src/Drastic.TrayWindow/WinUITrayWindow.Windows.cs
+++ src/Drastic.TrayWindow/WinUITrayWindow.Windows.cs
@@ -2,9 +2,11 @@
 // Copyright (c) Drastic Actions. All rights reserved.
 // </copyright>
 
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Drastic.Tray;
 using Microsoft.UI.Windowing;
+using WinRT.Interop;
 
 namespace Drastic.TrayWindow
 {
@@ -23,7 +25,8 @@ namespace Drastic.TrayWindow
         /// </summary>
         /// <param name="icon">Tray Icon.</param>
         /// <param name="options">Tray Icon Options.</param>
-        public WinUITrayWindow(TrayIcon icon, TrayWindowOptions options)
+        /// <param name="hideWindowFromTaskbar">Hides the Tray Window from the Taskbar and Alt-Tab. Defaults to True.</param>
+        public WinUITrayWindow(TrayIcon icon, TrayWindowOptions options, bool hideWindowFromTaskbar = true)
         {
             this.icon = icon;
             this.options = options;
@@ -38,6 +41,14 @@ namespace Drastic.TrayWindow
             }
 
             this.appWindow.Hide();
+
+            if (hideWindowFromTaskbar)
+            {
+                IntPtr hWnd = WindowNative.GetWindowHandle(this);
+                int exStyle = (int)GetWindowLong(hWnd, (int)GetWindowLongFields.GWL_EXSTYLE);
+                exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+                SetWindowLong(hWnd, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+            }
         }
 
         /// <summary>
@@ -72,5 +83,69 @@ namespace Drastic.TrayWindow
         {
             this.appWindow.Hide();
         }
+
+        // From https://stackoverflow.com/questions/74261765/remove-the-window-from-the-taskbar-in-winui-3
+        #region Window styles
+        [Flags]
+        private enum ExtendedWindowStyles
+        {
+            // ...
+            WS_EX_TOOLWINDOW = 0x00000080,
+            // ...
+        }
+
+        private enum GetWindowLongFields
+        {
+            // ...
+            GWL_EXSTYLE = (-20),
+            // ...
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        private static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+            // Win32 SetWindowLong doesn't clear error on success
+            SetLastError(0);
+
+            if (IntPtr.Size == 4)
+            {
+                // use SetWindowLong
+                Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(tempResult);
+            }
+            else
+            {
+                // use SetWindowLongPtr
+                result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0))
+            {
+                throw new System.ComponentModel.Win32Exception(error);
+            }
+
+            return result;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        private static int IntPtrToInt32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        private static extern void SetLastError(int dwErrorCode);
+        #endregion
     }
 }
