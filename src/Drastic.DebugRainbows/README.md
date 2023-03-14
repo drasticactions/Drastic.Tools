d30beeeb10d8fc172d7c7950b9dc342861806f06
Timothy Miller <innerlogic4321@gmail.com>
2023-03-14 16:16:59 +0900

    Add DebugRainbows

---
+++ src/Drastic.DebugRainbows/README.md
@@ -0,0 +1,4 @@
+[![NuGet Version](https://img.shields.io/nuget/v/Drastic.DebugRainbows.svg)](https://www.nuget.org/packages/Drastic.DebugRainbows/) ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+# Drastic.DebugRainbows
+
---
+++ src/Drastic.DebugRainbows/ViewExtensions.iOS.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ViewExtensions.iOS.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using ObjCRuntime;
+
+namespace Drastic.DebugRainbows
+{
+    public static class ViewExtensions
+    {
+        private static readonly Random randomGen = new Random();
+
+        private static readonly UIColor color = new UIColor(randomGen.Next(0, 255), randomGen.Next(0, 255), randomGen.Next(0, 255), 255);
+
+        public static void AddDebugRainbows(this UIView view)
+        {
+            foreach (var subview in view.Subviews)
+            {
+                subview.BackgroundColor = color;
+                subview.AddDebugRainbows();
+            }
+        }
+
+        public static void AddDebugRainbows(this UIViewController viewController)
+        => viewController.View?.AddDebugRainbows();
+    }
+}
